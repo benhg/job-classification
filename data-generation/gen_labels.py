32542dad94dd915e0e720be553136a18073d353d
Ben Glick <glick@c993388.local>
2019-03-11 16:20:38 -0700

    adding duplication protection to labelling process

--- data-generation/gen_labels.py
+++ data-generation/gen_labels.py
@@ -11,6 +11,7 @@ It should also generate/add to the data file which is generated
 """
 import glob
 import csv
+import pandas as pd
 
 DATA_FILE = "job_labels.csv"
 
@@ -21,7 +22,6 @@ def load_scripts(script_dir="scripts"):
 
 def label_script(script, label):
 	with open(DATA_FILE, "a") as fh:
-		print(script.split("/")[-1])
 		writer = csv.DictWriter(fh, fieldnames=["file", "class"])
 		writer.writerow({"file": script.split("/")[-1], "class": label})
 
@@ -31,9 +31,14 @@ def get_script_data(script):
 
 def get_and_write_all():
 	scripts = load_scripts()
+	df = pd.read_csv(DATA_FILE)
+	prev_scripts = df.file
+	for s in prev_scripts:
+		scripts.remove("scripts/"+s)
 	for script1 in scripts:
-		script, cat = get_script_data(script1)
-		label_script(script, cat)
+		if script1.split('/')[-1].strip() not in prev_scripts:
+			script, cat = get_script_data(script1)
+			label_script(script, cat)
 
 if __name__ == '__main__':
 	get_and_write_all()

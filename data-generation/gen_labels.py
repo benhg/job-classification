784778dacc66793c679b2a979123a459d2d2d7ca
Ben Glick <glick@lclark.edu>
2019-03-11 08:53:06 -0700

    getting started with data generation

---
+++ data-generation/gen_labels.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/env python3
+""""
+This file is intended to be a helper script for 
+me to hand-label my python scripts with their
+classifications. It will tell me the name of a
+python file, and as me for a classification. That way,
+I should be able to label many many scripts relatively
+quickly. 
+
+It should also generate/add to the data file which is generated
+"""
---
+++ data-generation/gen_tags.py
@@ -0,0 +1,9 @@
+!  # /usr/local/bin/env python3
+"""
+This file is intended to generate tags on script information
+What I mean by "tags" is derivative features - that is, features
+which are generated as derivatives of other scripts. One example of
+such a feature would be source lines of code, or number of loops.
+This script should process a python "job" (mostly just python scripts for now)
+and add those "tags" to a data file about them.
+"""

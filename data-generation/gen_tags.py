60b3bb069932313e6245945c21515225008afaef
Ben Glick <glick@lclark.edu>
2019-03-12 17:27:03 -0700

    playing with abstract syntax trees

--- data-generation/gen_tags.py
+++ data-generation/gen_tags.py
@@ -12,13 +12,73 @@ import pprint
 
 
 def get_for_count(script):
+    numfors = 0
     tree = ""
     with open(f"scripts/{script}" , "r") as fh:
         tree = ast.parse(fh.read())
-    pprint.pprint(ast.dump(tree))
+    for stmt in ast.walk(tree):
+        if isinstance(stmt, ast.For):
+            numfors += 1
+    return numfors
+
+def get_while_count(script):
+    whiles = 0
+    tree = ""
+    with open(f"scripts/{script}" , "r") as fh:
+        tree = ast.parse(fh.read())
+    for stmt in ast.walk(tree):
+        if isinstance(stmt, ast.While):
+            whiles += 1
+    return whiles
+
+def get_loop_count(script):
+    return get_for_count(script) + get_while_count(script)
+
+def get_function_defs(script):
+    defs = 0
+    tree = ""
+    with open(f"scripts/{script}" , "r") as fh:
+        tree = ast.parse(fh.read())
+    for stmt in ast.walk(tree):
+        if isinstance(stmt, ast.FunctionDef):
+            defs += 1
+            print(stmt)
+    return defs
+
+
+def get_function_calls(script):
+    calls = 0
+    tree = ""
+    with open(f"scripts/{script}" , "r") as fh:
+        tree = ast.parse(fh.read())
+    for stmt in ast.walk(tree):
+        if isinstance(stmt, ast.Call):
+            calls += 1
+            print(stmt)
+    return calls
+
+
+def get_loads(script):
+    loads = 0
+    tree = ""
+    with open(f"scripts/{script}" , "r") as fh:
+        tree = ast.parse(fh.read())
+    for stmt in ast.walk(tree):
+        if isinstance(stmt, ast.Load):
+            loads += 1
+            print(stmt)
+    return loads
+
+def get_deepest_stmt(script, depth=0):
+    depth = 0
+    tree = ""
+    with open(f"scripts/{script}" , "r") as fh:
+        tree = ast.parse(fh.read())
+    for stmt in ast.walk(tree):
+        pass
 
 
 
 if __name__ == '__main__':
-    get_for_count("rudpclient.py")
+    print(get_function_calls("p7.py"))
 

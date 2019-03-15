2b23a78219cad6d17ca1a1870f79587dbbc4467c
Ben Glick <glick@lclark.edu>
2019-03-14 23:07:38 -0700

    adding tensorflow neural net. It's not working, but that's just beaucse i can't figure out how to get it to train

---
+++ tensorflow_neuralnet.py
@@ -0,0 +1,111 @@
+"""
+Now we use the big guns. This is going to be a little neural net,
+with (for now) five features, two hidden layers of size 16, and 
+an output layer of size five
+
+"""
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+tf.enable_eager_execution()
+features = pd.read_csv("data-generation/jobs_features.csv")
+labels = pd.read_csv("data-generation/job_labels.csv")
+
+all_data = features.merge(labels, left_on="name", right_on="file")
+all_data['class'] = pd.factorize(all_data['class'])[0]
+
+features = ["loops", "fdefs", "fcalls", "loads", "asmts"]
+
+training_dataset = (
+    tf.data.Dataset.from_tensor_slices(
+        (
+            tf.cast(all_data[features].values, tf.float32),
+            tf.cast(all_data['class'].values, tf.int32)
+        )
+    )
+)
+
+mnist = training_dataset
+
+
+# Parameters
+learning_rate = 0.1
+num_steps = 500
+batch_size = 128
+display_step = 100
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+num_input = 5 # MNIST data input (img shape: 28*28)
+num_classes = 5 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+
+# Create model
+def neural_net(x):
+    # Hidden fully connected layer with 256 neurons
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    # Hidden fully connected layer with 256 neurons
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Construct model
+logits = neural_net(tf.cast(all_data[features].values, tf.float32))
+prediction = tf.nn.softmax(logits)
+
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=tf.cast(all_data[features].values, tf.int32)))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(tf.cast(all_data['class'].values, tf.int32), 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Start training
+iter = mnist.make_one_shot_iterator()
+
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
+
+    for step in range(1, num_steps+1):
+        batch_x, batch_y = mnist.batch(10)
+        # Run optimization op (backprop)
+        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+        if step % display_step == 0 or step == 1:
+            # Calculate batch loss and accuracy
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+                                                                 Y: batch_y})
+            print("Step " + str(step) + ", Minibatch Loss= " + \
+                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.3f}".format(acc))
+
+    print("Optimization Finished!")
+
+    # Calculate accuracy for MNIST test images
+    print("Testing Accuracy:", \
+        sess.run(accuracy, feed_dict={X: mnist.test.images,
+                                      Y: mnist.test.labels}))

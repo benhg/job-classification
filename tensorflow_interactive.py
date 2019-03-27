b9cad6194faf1d4bcd6871e3bb198fb49bde21f2
Ben Glick <glick@lclark.edu>
2019-03-27 13:13:26 -0700

    adding interactive mode

---
+++ tensorflow_interactive.py
@@ -0,0 +1,7 @@
+"""
+I want to make a little interface so i can do live testing
+for demos and such. The neural network should be exactly the same
+as with the other one, at least for now, but I want to be able to tell it
+where a script is. Maybe in the future, i will have it read from a library
+of models I come up with.
+"""
